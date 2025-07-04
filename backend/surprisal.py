30c5a7e648c9d0aaaa505d6a60514330cb765d93
wobblybits <somebody@wobblybits.blog>
2025-07-04 17:06:58 -0400

    fixes

--- backend/surprisal.py
+++ backend/surprisal.py
@@ -3,10 +3,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/process/')
-def app():
+def backend():
     text = request.args.get('text')
     return "{\"received\": " + text + ", \"response\": \"Hello, World!\"}";
 
 @app.route('/')
-def index():
+def frontend():
     return render_template('../frontend/index.html')
