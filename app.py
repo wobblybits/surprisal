e249c674902b66ec2fa2ee927969eb5b9baa2651
wobblybits <somebody@wobblybits.blog>
2025-07-04 17:40:40 -0400

    post

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/process/')
+@app.route('/process/', methods=['POST'])
 def backend():
-
     text = request.args.get('text', "No text provided")
     return "{\"received\": " + text + ", \"response\": \"Hello, World!\"}";
 
