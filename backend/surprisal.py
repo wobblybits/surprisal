dd608d53178b57d00206fc8631582a23006ab52b
wobblybits <somebody@wobblybits.blog>
2025-07-04 16:44:45 -0400

    fixes

--- backend/surprisal.py
+++ backend/surprisal.py
@@ -3,9 +3,6 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def app():
     text = request.args.get('text')
     return "{\"received\": " + text + ", \"response\": \"Hello, World!\"}";
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000) 
