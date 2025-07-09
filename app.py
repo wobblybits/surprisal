cc785433046819d1f940bc67299731256a2abb46
eayhkim <kim.a.elise@gmail.com>
2025-07-09 11:27:07 -0700

    word freq and length modulate volume and duration of melody

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import json
 import surprisal
-
+import wordfreq
 app = Flask(__name__)
 
 counter = 0
@@ -11,10 +11,22 @@ g = surprisal.AutoHuggingFaceModel.from_pretrained(model_id="gpt2")
 def backend():
     text = request.args.get('text', "No text provided")
     surps = [*g.surprise([text])]
-    return json.dumps({
-        surprisals: [*surps[0].surprisals],
-        tokens: [*surps[0].tokens],
-    })
+    surprisals = [round(i) for i in [*surps[0].surprisals]]
+
+    tokens = [*surps[0].tokens]
+    tokens_clean = [i.lstrip("Ġ") for i in tokens]
+
+    lengths = [len(i) for i in tokens_clean]
+
+    frequencies = [wordfreq.zipf_frequency(i, "en") for i in tokens_clean]
+    # setting minimum frequency value at 0.5 to avoid silent notes
+    frequencies_inverted = [max((8 - i), 0.5) for i in frequencies]
+    return json.dumps({"surprisals": surprisals,
+                    "tokens": tokens_clean,
+                    "lengths": lengths,
+                    "frequencies": frequencies,
+                    "frequencies_inverted": frequencies_inverted})
+    
 
 @app.route('/')
 def frontend():
@@ -25,3 +37,6 @@ def test():
     g = surprisal.AutoHuggingFaceModel.from_pretrained(model_id="gpt2")
     surps = [*g.surprise(["Hello, World!"])]
     return json.dumps({"surprisals": surps[0].surprisals, "tokens": surps[0].tokens})
+
+if __name__ == "__main__":
+    app.run(debug=True)
