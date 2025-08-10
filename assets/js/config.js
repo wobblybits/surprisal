a64fd419ff857ac5d3950828d118893240d757ce
wobblybits <somebody@wobblybits.blog>
2025-08-10 11:49:48 -0500

    Default config.js for smallm

--- assets/js/config.js
+++ assets/js/config.js
@@ -114,7 +114,7 @@ export const applicationSettings = {
     minKey: 1,
     maxKey: 9
   },
-  defaultSettings: ["THEREMIN", "MAJOR-7", "FLAN"],
+  defaultSettings: ["THEREMIN", "MAJOR-7", "SMOLLM"],
   defaultVector: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 };
 
